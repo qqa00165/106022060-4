bf2cb069e14869966ac7f8944ea808ed34ce2984
qqa00165 <qqa00165@gmail.com>
2017-09-25 15:49:35 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        int a,b,c,d,e;
+        int x;
+        Scanner in=new Scanner(System.in);
+        System.out.print("請輸入x=");
+        x=in.nextInt();
+        a=x/10000;
+        b=x/1000%10;
+        c=x/100%10;
+        d=x/10%10;
+        e=x%10;
+        System.out.printf("%d %d %d %d %d",a,b,c,d,e);
+        in.close();
+	// write your code here
+    }
+}
